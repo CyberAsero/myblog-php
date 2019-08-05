47ed11f0e7d789370715123bfd68876d5a97f69b
Marlon Macatuno <50572037+CyberAsero@users.noreply.github.com>
2019-08-05 13:32:36 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+include "auth/db.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Android App PHP MYSQL</title>
+</head>
+<body>
+    <h1>Android App PHP MYSQL</h1>
+</body>
+</html>
